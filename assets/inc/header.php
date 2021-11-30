6b8798ee166b16a20dc6ed02f08b8376a4a7be8b
JackHeeney <jackheeney1@googlemail.com>
2021-11-30 12:07:19 +0000

    first commit

---
+++ assets/inc/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="robots" content="all,follow">
+
+    <!-- ======= Meta tags ======== -->
+    <title><?php echo $title; ?></title>
+    <meta name="keywords" content="<?php echo $meta['keywords']; ?>" />
+    <meta name="description" content="<?php echo $meta['description']; ?>" />
+
+    <!-- ======= Favicons ======== -->
+    <link href="<?php echo SITE_ADDR ?>/assets/img/favicon_1.png" rel="icon">
+    <link href="<?php echo SITE_ADDR ?>/assets/img/favicon_1.png" rel="apple-touch-icon">
+
+    <!-- ======= Bootstrap CSS ======== -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- ======= Font Awesome CSS ======== -->
+    <link async rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+        integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous" />
+
+    <!-- ======= Swiper JS ======== -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+    <!-- ======= AOS (animation) ======== -->
+    <link href="<?php echo SITE_ADDR; ?>/assets/css/vendor/aos.css" rel="stylesheet">
+
+    <!-- ======= Main CSS ======== -->
+    <link rel="stylesheet" href="<?php echo SITE_ADDR ?>/public/style.css">
+
+</head>
+
+<body>
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+// Change page title and meta tags here
+$title = "Microsoft Certified Training | Robust IT Training";
+$meta['keywords'] = "";
+$meta['description'] = "";
+
+// Social media meta tags
+$meta['og:title'] = "Microsoft Certified Training | Robust IT Training";
+$meta['og:description'] = "";
+$meta['og:image'] = "";
+$meta['og:url'] = "";
+
+?>
+
+<?php include './assets/inc/header.php' ?>
+<?php include './assets/inc/components/nav.php' ?>
+
+
+
+
+
+<?php include './assets/inc/footer.php' ?>
